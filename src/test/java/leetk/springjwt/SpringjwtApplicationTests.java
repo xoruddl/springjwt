b9f560f2279aceeefd4430584f035418d1be2f10
itaekyung <taeyun1411@gmail.com>
2025-08-25 17:08:57 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/leetk/springjwt/SpringjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package leetk.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
