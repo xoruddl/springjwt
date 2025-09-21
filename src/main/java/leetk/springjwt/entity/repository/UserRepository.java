05b1495663d033b29c16dc12d2d34679fc635d7a
itaekyung <taeyun1411@gmail.com>
2025-09-21 16:27:45 +0900

    feat: jwt 5강, 디비연결, 엔티티 생성

---
+++ src/main/java/leetk/springjwt/entity/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package leetk.springjwt.entity.repository;
+
+import leetk.springjwt.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+}
